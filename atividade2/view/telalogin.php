83d8cbac5fe4aa3319f1ca2e474c42ed23ea138d
CTI Guarulhos <ctiguarulhos@ifsp.edu.br>
2024-03-13 20:05:23 -0300

    subindo tela login

---
+++ atividade2/view/telalogin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/estilo.css">
+    <title>Document</title>
+</head>
+<body>
+<h1 class="titulologin">Acesso ao sistema</h1>
+<form action="" method="">
+    <fieldset id="cxlogin">
+        <legend>Sistema de Cadastro</legend>
+        Login:
+        <input type="email" name="cxemail"> <br>
+        Senha:
+        <input type="password" name="cxsenha"> <br><br>
+        <input type="submit" name="Acessar"> <br>
+        <figure id="cadeado">
+            <img src="../img/cadeado.png" alt="cadeado" class="figura1">
+
+
+        </figure>
+
+
+    </fieldset>
+</form>
+
+    
+</body>
+</html>
