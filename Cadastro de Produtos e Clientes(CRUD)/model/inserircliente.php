e2dded80038b2f74ed4fc5130c7da6e79a5efb12
dib10 <caiodib10@gmail.com>
2024-03-16 22:02:34 -0300

    personalizacao das paginas de erro

--- Cadastro de Produtos e Clientes(CRUD)/model/inserircliente.php
+++ Cadastro de Produtos e Clientes(CRUD)/model/inserircliente.php
@@ -1,5 +1,5 @@
 <?php
-
+$message = "";
 if($_POST['cxnome']!="")
 {
     include_once '../factory/conexao.php';
@@ -7,14 +7,27 @@ if($_POST['cxnome']!="")
     $email = $_POST['cxemail'];
     $sql = "INSERT INTO tbcliente (nome,email) values ('$nome','$email')";
     $query = mysqli_query($conn,$sql);
-    echo "Dados cadastrados com sucesso";
+    $message = "Dados cadastrados com sucesso!";
 }
 else
 {
-    echo "Dados não cadastrados";
+    $message = "Dados não cadastrados!";
 }
 ?>
 
-
-
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $message; ?></title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body id="excluirclientefundo">
+<div id="exclusaoconcluida">
+    <h1><?php echo $message; ?></h1>
+    <a href="listarcliente.php" id="botaosubmit">Voltar</a>
+</div>
+    
+</body>
+</html>
