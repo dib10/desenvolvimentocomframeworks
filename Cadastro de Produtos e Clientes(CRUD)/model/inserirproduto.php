c3818bd0e0893b53689cef9a0fd28ad9766d284a
dib10 <caiodib10@gmail.com>
2024-03-17 02:46:38 -0300

    adição da icon na aba do navegador em ambas seçoes

--- Cadastro de Produtos e Clientes(CRUD)/model/inserirproduto.php
+++ Cadastro de Produtos e Clientes(CRUD)/model/inserirproduto.php
@@ -25,6 +25,7 @@ else
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $message; ?></title>
     <link rel="stylesheet" href="../css/estilo.css">
+
 </head>
 <body id="excluirclientefundo">
 <div id="exclusaoconcluida">
